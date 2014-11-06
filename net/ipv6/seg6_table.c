a396f9263725733f25af423c03a4f3abdb549053
David Lebrun <target0@geeknode.org>
2014-11-06 17:44:35 +0100

    SR-IPv6: added copyright information

--- net/ipv6/seg6_table.c
+++ net/ipv6/seg6_table.c
@@ -1,3 +1,16 @@
+/*
+ *  SR-IPv6 implementation -- LPM tree
+ *
+ *  Author:
+ *  David Lebrun <david.lebrun@uclouvain.be>
+ *
+ *
+ *  This program is free software; you can redistribute it and/or
+ *      modify it under the terms of the GNU General Public License
+ *      as published by the Free Software Foundation; either version
+ *      2 of the License, or (at your option) any later version.
+ */
+
 #include <linux/errno.h>
 #include <linux/types.h>
 #include <linux/socket.h>
