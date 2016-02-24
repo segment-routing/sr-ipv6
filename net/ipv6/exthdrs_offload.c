3d16076f288b8eebd0df9dd6e0bb0b8ecd40102f
David Lebrun <target0@geeknode.org>
2016-02-24 16:58:32 +0100

    sr-ipv6: remove srh offloading
    When receiving a packet with an inline SRH and GRO enabled, and the host is
    the final destination of the packet but the active segment is not the last
    segment (i.e., the packet will jump one or more times to the looped_back
    label in ipv6_srh_receive() and the DA of the packet will change),
    with an inner protocol that verify checksums and support GRO (e.g., TCP),
    the protocol's gro_receive function will set the skb->csum_bad = 1 because
    the current pseudo-header does not match the initial/final pseudo-header as
    the current DA of the packet is not the final one. The packet will be flushed
    upward by GRO with csum_bad=1, causing it to fail the subsequent
    skb_checksum_init by the inner protocol.
    
    This commit removes the INET6_PROTO_GSO_EXTHDR flag from the ROUTING protocol
    as no assumption can be made on such packets.

--- net/ipv6/exthdrs_offload.c
+++ net/ipv6/exthdrs_offload.c
@@ -12,10 +12,6 @@
 #include <net/protocol.h>
 #include "ip6_offload.h"
 
-static const struct net_offload rthdr_offload = {
-	.flags		=	INET6_PROTO_GSO_EXTHDR,
-};
-
 static const struct net_offload dstopt_offload = {
 	.flags		=	INET6_PROTO_GSO_EXTHDR,
 };
@@ -24,18 +20,7 @@ int __init ipv6_exthdrs_offload_init(void)
 {
 	int ret;
 
-	ret = inet6_add_offload(&rthdr_offload, IPPROTO_ROUTING);
-	if (ret)
-		goto out;
-
 	ret = inet6_add_offload(&dstopt_offload, IPPROTO_DSTOPTS);
-	if (ret)
-		goto out_rt;
 
-out:
 	return ret;
-
-out_rt:
-	inet6_del_offload(&rthdr_offload, IPPROTO_ROUTING);
-	goto out;
 }
